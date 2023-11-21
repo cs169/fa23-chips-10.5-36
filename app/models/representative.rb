26e110304390ddf58f620a601e7cef2f3d12cda4
Christina Huynh <cvhuynh1777@berkeley.edu>
2023-11-21 22:41:09 +0000

    completed profile

--- app/models/representative.rb
+++ app/models/representative.rb
@@ -4,26 +4,18 @@ class Representative < ApplicationRecord
   has_many :news_items, dependent: :delete_all
 
   def self.civic_api_to_representative_params(rep_info)
-    reps = []
+    rep_info.officials.map.with_index do |official, index|
+      office = rep_info.offices.find { |o| o.official_indices.include? index }
+      next unless office
 
-    rep_info.officials.each_with_index do |official, index|
-      ocdid_temp = ''
-      title_temp = ''
-
-      rep_info.offices.each do |office|
-        if office.official_indices.include? index
-          title_temp = office.name
-          ocdid_temp = office.division_id
-        end
-      end
-
-      next if Representative.find_by(name: official.name, title: title_temp).present?
-
-      rep = Representative.create!({ name: official.name, ocdid: ocdid_temp,
-          title: title_temp })
-      reps.push(rep)
-    end
-
-    reps
+      Representative.create!({
+                               name:            official.name,
+                               ocdid:           office.division_id,
+                               title:           office.name,
+                               contact_address: official.address&.first&.line1,
+                               political_party: official.party,
+                               photo_url:       official.photo_url
+                             })
+    end.compact
   end
 end
